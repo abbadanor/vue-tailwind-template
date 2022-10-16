231d79d89943ef7eae4f025b0ff5627d43e3f416
Adam Nord <adam.nord@abbgymnasiet.se>
2022-10-16 17:27:35 +0200

    added login and register pages

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -7,7 +7,7 @@ const counter = useCounterStore()
     <div class="absolute h-full w-full flex flex-col justify-center items-center">
         <h1 class="font-bold text-3xl mb-2">Vite⚡ + Vue👀 + Tailwind🌊 + Pinia🍍</h1>
         <button type="button"
-            class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800"
+            class="text-indigo-700 hover:text-white border border-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-indigo-500 dark:text-indigo-500 dark:hover:text-white dark:hover:bg-indigo-600 dark:focus:ring-indigo-800"
             @click="counter.count++">count is {{ counter.count }}</button>
     </div>
 </template>
