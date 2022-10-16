064c76a10c499197a58746e62485be1ef9cf328a
Adam Nord <adam.nord@abbgymnasiet.se>
2022-10-16 17:01:18 +0200

    centrerade vite i navbaren

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="mx-auto px-4 w-full">
-        <div class="h-16 flex justify-between items-center border-b-2 border-gray-100 mx-auto px-4 w-full">
-            <div class="flex gap-x-4 items-center">
+    <div class="px-4">
+        <div class="h-16 flex justify-center items-center border-b-2 border-gray-100 w-full relative">
+            <div class="flex gap-x-4 items-center absolute left-0">
                 <router-link active-class="text-gray-900" class="font-medium text-gray-500 hover:text-gray-900" to="/">Home</router-link>
                 <router-link active-class="text-gray-900" class="font-medium text-gray-500 hover:text-gray-900" to="/about">About</router-link>
                 <router-link active-class="text-gray-900" class="font-medium text-gray-500 hover:text-gray-900" to="/todo">To-do</router-link>
             </div>
             <div>
-                <p class="font-medium text-lg text-gray-900">Vite</p>
+                <router-link to="/" class="font-medium text-lg text-gray-900">Vite</router-link>
             </div>
-            <div class="flex gap-x-4 items-center">
-                <router-link class="font-medium text-gray-500 hover:text-gray-900" to="/login">Login</router-link>
-                <div class="text-white px-4 py-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg focus:outline-none">
+            <div class="flex gap-x-4 items-center absolute right-0">
+                <router-link class="text-sm font-medium text-gray-500 hover:text-gray-900" to="/login">Login</router-link>
+                <div class="text-white text-sm px-4 py-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg focus:outline-none">
                     <router-link to="/register">Register</router-link>
                 </div>
             </div>
