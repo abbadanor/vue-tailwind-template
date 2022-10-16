6c65f1c9f772fef7315e6168cba1811189c835bf
Adam Nord <adam.nord@abbgymnasiet.se>
2022-10-16 16:50:04 +0200

    edited todo

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,7 +1,6 @@
 <template>
     <h1 class="font-bold text-3xl">To-do</h1>
     <ul class="ml-4">
-        <li class="list-disc">Fixa så att <code>class="grow"</code> inte måste vara inuti varje component.</li>
         <li class="list-disc">Centrera "Vite" i navbaren</li>
         <li class="list-disc"><code>vite preview</code> funkar bara när <code>base</code> är <code>./</code> och inte <code>/vue-tailwind-template/</code>. Därför funkar inte github pages. Måste undersökas</li>
     </ul>
